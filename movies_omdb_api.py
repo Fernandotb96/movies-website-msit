978ff12e06362816ecdfeab71ab4191b18b4b367
Fernando Tomas <fernandotb96@gmail.com>
2026-07-02 15:18:56 +0200

    movies_omdb_api.py file added for API integration

---
+++ movies_omdb_api.py
@@ -0,0 +1,29 @@
+import os
+import dotenv
+import requests
+
+# Get API Key
+dotenv.load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+API_URL = "https://www.omdbapi.com/"
+
+
+def get_movie_from_api(title):
+    params = {"apikey": API_KEY, "t": title}
+    try:
+        response = requests.get(API_URL, params=params)
+        response.raise_for_status()
+        data = response.json()
+        if data["Response"] == "True":
+            return data
+        return None
+    except Exception as e:
+        print(f"Error with the API: {e}")
+        return None
+
+
+test_movie = get_movie_from_api("TITLE HERE")
+print(test_movie["Title"])
+print(test_movie["Year"])
+print(test_movie["imdbRating"])
